7905bbb71394d5cdea65e11bd9e6c1b69b8f9050
Jayaraman Pancharathinam <jayaram.mailbox@gmail.com>
2021-10-29 01:43:00 +0900

    Improved MyThread class; added factory method to create and start threads.

--- src/edu/java/basic/threading/MainThread.java
+++ src/edu/java/basic/threading/MainThread.java
@@ -2,14 +2,9 @@ package edu.java.basic.threading;
 
 public class MainThread {
     public static void main(String[] args) {
+        System.out.println("Main thread starting...");
         //Step 1 Constructing MyThread object
-        MyThread mt = new MyThread("Child #1");
-
-        //Step 2 Construct a thread from MyThread object
-        Thread mainThread = new Thread(mt);
-
-        // Finally start the thread
-        mainThread.start();
+        MyThread mt = MyThread.createAndStart ("Child #1");
 
         for (int i = 0; i < 50; i++) {
             System.out.print(".");
--- src/edu/java/basic/threading/MyThread.java
+++ src/edu/java/basic/threading/MyThread.java
@@ -2,23 +2,30 @@ package edu.java.basic.threading;
 
 // Creating thread by implementing runnable interface
 public class MyThread implements Runnable{
-    String threadName;
+    Thread thread;
 
     MyThread(String name) {
-        threadName = name;
+        thread = new Thread(this, name);
+    }
+
+    // A factory method that creates and starts the thread
+    public static MyThread createAndStart(String name) {
+        MyThread myThread = new MyThread(name);
+        myThread.thread.start();
+        return myThread;
     }
 
     // Entry point for thread
     public void run() {
-        System.out.println(threadName + " starting...");
+        System.out.println(thread.getName() + " starting...");
         try {
             for (int count = 0; count < 10; count++) {
                 Thread.sleep(400);
-                System.out.println("In " + threadName + ", count is " + count);
+                System.out.println("In " + thread.getName() + ", count is " + count);
             }
         } catch (InterruptedException ex) {
-            System.out.println("Interrupted thread:" + threadName);
+            System.out.println("Interrupted thread:" + thread.getName());
         }
-        System.out.println("Terminating thread: " + threadName);
+        System.out.println("Terminating thread: " + thread.getName());
     }
 }
